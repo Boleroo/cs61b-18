197b557cc3d27b55b808a7e8a57d1edf6a02680b
ycy <2423398379@qq.com>
2024-09-25 21:42:50 +0800

    completed first part of lab1

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+/*
+1. All code in Java must be part of a class
+2. For code to run we need public static void main(String[] args)
+ */
